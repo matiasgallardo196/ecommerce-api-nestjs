46329049dda12072d8e421ee1dd7b9b72ae0e13d
MatiasGGallardo <matiasgallardo196@gmail.com>
2025-08-03 13:54:57 +1000

    feat: Refactor typeorm configuration to support production environment with DATABASE_URL

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -2,20 +2,35 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { registerAs } from '@nestjs/config';
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } from './env.loader';
 
-const config = {
-  type: 'postgres',
-  database: DB_NAME,
-  host: DB_HOST,
-  port: DB_PORT,
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  autoLoadEntities: true,
-  entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: ['dist/migrations/*{.ts,.js}'],
-  synchronize: true,
-  logging: true,
-  //dropSchema: true,
-};
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = isProduction
+  ? {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      autoLoadEntities: true,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/*{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+    }
+  : {
+      type: 'postgres',
+      host: DB_HOST,
+      port: DB_PORT,
+      username: DB_USERNAME,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      autoLoadEntities: true,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/*{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+      //dropSchema: true,
+    };
 
 export default registerAs('typeorm', () => config);
 
